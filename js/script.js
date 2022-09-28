f202ff805acd5209087529cccfbdf8505a69c583
Aleksandra Bukalskai <bukalska.aleksandra@gmail.com>
2022-09-28 17:12:53 +0100

    Add class active to the correct article

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -12,7 +13,7 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
@@ -25,11 +26,19 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    const clickedLinkAttribute = clickedElement.getAttribute('href');
+    console.log(clickedLinkAttribute);
 
-    /* add class 'active' to the correct article */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const matchingArticle = document.querySelector(clickedLinkAttribute);
+    console.log(matchingArticle);
+
+    /* [DONE] add class 'active' to the correct article */
+
+    matchingArticle.classList.add('active');
   }
 
   const links = document.querySelectorAll('.titles a');
